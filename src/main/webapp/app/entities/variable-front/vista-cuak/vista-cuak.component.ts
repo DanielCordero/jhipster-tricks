7a350bbdf040277453b4a8edc5ca6e986f51ec9e
daniel <yo@danielcordero.ml>
2018-08-20 13:21:44 -0500

    Agrega un mini módulo para hacer inyección

---
+++ src/main/webapp/app/entities/variable-front/vista-cuak/vista-cuak.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-vista-cuak',
+  templateUrl: './vista-cuak.component.html',
+  styles: []
+})
+export class VistaCuakComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
